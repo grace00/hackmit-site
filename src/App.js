218a374f6a56ac825fe69ab5302ab07e3056e9e5
Grace <gkzhou@stanford.edu>
2020-09-19 00:27:57 -0400

    Set up router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Switch, HashRouter, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <HashRouter>
+          <NavBar />
+          <Switch>
+            {/*<Route exact path='/' component={MainPage} />*/}
+          </Switch>
+
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
           </p>
-        <button class="ui button" role="button">Btn with Semantic-UI</button>
+          <button class="ui button" role="button">Btn with Semantic-UI</button>
+        
+        </HashRouter>
       </div>
     );
   }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react'
+
+export default function NavBar() {
+  return (
+    <Menu secondary>
+        <Link to="/">
+          <Menu.Item name='home' />
+        </Link>
+        <Link to="/">
+          <Menu.Item name='businesses' />
+        </Link>
+        <Link to="/">
+          <Menu.Item name='Post Your Business' />
+        </Link>
+      </Menu>
+  )
+}
