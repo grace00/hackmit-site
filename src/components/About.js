9dda8c1f278873d701ab9b52cc3f3bc8873e2aaa
Karra <karra@MacBook-Pro.local>
2020-09-19 16:32:03 -0700

    Added chatbot

--- src/components/About.js
+++ src/components/About.js
@@ -69,6 +69,9 @@ class About extends Component {
                      ></Project>
                 ))}
         </div>
+        <div>
+       <a target="_blank" href="https://covid-assistant-simple-mythili.mybluemix.net">Policies</a>
+        </div>
         </div>);
     }
 }
