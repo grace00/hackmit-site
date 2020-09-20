0f85f949c93196b58f6a76519de78183595b6b55
Karra <karra@MacBook-Pro.local>
2020-09-19 22:39:31 -0700

    minor changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,7 +18,8 @@ class About extends Component {
     }
     render() {
         return (<div>
-            {/* <h1 className='heading-background'>BEEYOND</h1> */}
+            {
+             <h1 className='heading-background'>BEEYOND</h1> }
             <div className='splash-section'>
                 <Container className="splash-content-container">
                     <Grid>
@@ -77,9 +78,9 @@ class About extends Component {
                 ))}
             </div>
         </div>
-        
+
             <div className='about'>
-                <div>
+                <div className='about-content'>
                     <h1><Fade bottom cascade>About.</Fade></h1>
                     <Fade bottom>
                         <p>{data.abouttext}</p>
